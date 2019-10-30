226558cc1f2d56f864eb8e14fe86856b2f83369c
tanapat.so <tanapat.so@kbtg.tech>
2019-10-30 12:00:58 +0700

    test3 refactor

--- captcha.go
+++ captcha.go
@@ -1,11 +1,9 @@
 package captcha
 
+import (
+	"strconv"
+)
+
 func captcha(pattern int, leftOperand int, operator int, rightOperand int) string  {
-	if leftOperand == 1 {
-		return "1 + One"
-	}
-	if leftOperand ==3 {
-		return "3 + One"
-	}
-	return "2 + One"
+	return strconv.Itoa(leftOperand) + " + One"
 }
